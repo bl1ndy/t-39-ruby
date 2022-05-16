72a9f353c1b2bf18c46fc4aa54a7f0526a04b5c5
bl1ndy <ayoblindstone@gmail.com>
2022-05-16 18:36:52 +0300

    Add checks for invalid input

--- lesson-1/perfect_weight.rb
+++ lesson-1/perfect_weight.rb
@@ -6,16 +6,26 @@ puts "\n"
 puts "What's your name?"
 name = gets.chomp
 
+while name.empty?
+  puts 'Please, enter your name.'
+  name = gets.chomp
+end
+
 puts "\n"
 puts 'Please, enter your height (cm).'
 height = gets.chomp.to_i
+
+while height <= 0
+  puts 'Invalid data! Please, enter your height (cm).'
+  height = gets.chomp.to_i
+end
 
 perfect_weight = ((height - 110) * 1.15).round(1)
 
 answer = if perfect_weight.positive?
            "#{name}, the perfect weight for you is #{perfect_weight} kg."
          else
-           "Congrats, #{name}! Your weight is optimal."
+           "Your weight is optimal, #{name}!"
          end
 
 puts "\n"
