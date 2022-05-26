74980c59e482d4c7a25d4111fc6c522b7036abfb
bl1ndy <ayoblindstone@gmail.com>
2022-05-26 23:12:01 +0300

    Add check for absence of 'validate!' in target class

--- lesson-6/concerns/validatable.rb
+++ lesson-6/concerns/validatable.rb
@@ -7,4 +7,8 @@ module Validatable
   rescue
     false
   end
+
+  def validate!
+    raise "You should initialize method 'validate!' with your own validation logic in class: #{self.class}"
+  end
 end
