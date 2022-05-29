7f162cfe00cc16c11c7b6e68a35f0287d546bb87
bl1ndy <ayoblindstone@gmail.com>
2022-05-30 00:30:12 +0300

    Add capacity
    Add methods to validate and manipulate capacity

--- lesson-7/models/carriage.rb
+++ lesson-7/models/carriage.rb
@@ -1,13 +1,33 @@
 # frozen_string_literal: true
 
 require_relative '../concerns/producable'
+require_relative '../concerns/validatable'
 
 class Carriage
   include Producable
+  include Validatable
 
-  attr_reader :type
+  attr_reader :type, :capacity, :occupied
 
-  def initialize
+  def initialize(capacity = nil)
     @type = nil
+    @capacity = capacity
+    @occupied = 0
+
+    validate!
+  end
+
+  def fill(volume = nil)
+    @occupied += volume ||  1
+  end
+
+  def free
+    @capacity - @occupied
+  end
+
+  private
+
+  def validate!
+    raise TypeError, 'Invalid value for carriage capacity!' unless @capacity.is_a?(Integer) || @capacity.is_a?(Float)
   end
 end
