be5a81e6288229d70407b5421e2fc92aafda9472
bl1ndy <ayoblindstone@gmail.com>
2022-06-01 15:20:50 +0300

    Add files from previous lesson, edit rubocop config

---
+++ lesson-8/models/carriage.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative '../concerns/producable'
+require_relative '../concerns/validatable'
+
+class Carriage
+  include Producable
+  include Validatable
+
+  attr_reader :type, :capacity, :occupied
+
+  def initialize(capacity = nil)
+    @type = nil
+    @capacity = capacity
+    @occupied = 0
+
+    validate!
+  end
+
+  def fill(volume = nil)
+    result = @occupied + (volume || 1)
+
+    return if result > @capacity
+
+    @occupied += volume || 1
+  end
+
+  def free
+    @capacity - @occupied
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, 'Please set a carriage capacity' unless @capacity
+    raise TypeError, 'Invalid value for carriage capacity!' unless @capacity.is_a?(Integer) || @capacity.is_a?(Float)
+  end
+end
